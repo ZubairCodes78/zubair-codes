85d96061ae0fd154bd1490a1dd9a78f1a0c590f4
zugb743 <zatechnicalzubair@gmail.com>
2026-04-17 23:44:42 +0500

    added sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,36 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://zubaircodes.site",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://zubaircodes.site/contact",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://zubaircodes.site/automation",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://zubaircodes.site/web-development",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://zubaircodes.site/ai-content",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://zubaircodes.site/marketing",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://zubaircodes.site/portfolio",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://zubaircodes.site/about",
+      lastModified: new Date(),
+    },
+  ];
+}
